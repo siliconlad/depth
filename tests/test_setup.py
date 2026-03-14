608c540bcd62a7bed8a786b2f96bfbd3709bf923
Angus Stewart <siliconlad@protonmail.com>
2026-03-14 14:39:15 +0100

    feat: Add camera connection test

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 from tinygrad.device import Device
 from tinygrad.tensor import Tensor
@@ -7,3 +8,15 @@ def test_setup():
     assert Device.DEFAULT == "NV"
     result = (Tensor.randn(2,3) @ Tensor.randn(3,2)).numpy()
     assert isinstance(result, np.ndarray)
+
+
+@pytest.mark.camera
+def test_camera():
+    try:
+        import pyzed.sl as sl
+    except ImportError:
+        pytest.fail("Could not import pyzed. Run task sync-all to install.")
+
+    cam = sl.Camera()
+    status = cam.open(sl.InitParameters())
+    assert status == sl.ERROR_CODE.SUCCESS, "Faqiled to open camera"
